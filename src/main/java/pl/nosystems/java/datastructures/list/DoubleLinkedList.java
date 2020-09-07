42992ee1d0809ce7a631e8b39d0c67314aaf5d5d
Tomasz Jaranowski <t.jaranow@outlook.com>
2020-09-07 22:14:42 +0200

    Updated DoubleLinkedList to contain Add/Get/Remove First/Last. Added Unit tests for that.

--- src/main/java/pl/nosystems/java/datastructures/list/DoubleLinkedList.java
+++ src/main/java/pl/nosystems/java/datastructures/list/DoubleLinkedList.java
@@ -11,6 +11,248 @@ public final class DoubleLinkedList<T> {
     private DoubleLinkedListNode<T> lastNode;
     private int size = 0;
 
+    /**
+     * Adds element to collection.
+     * Equal to calling {@link #addLast(Object)}
+     *
+     * @param data element to be added
+     */
+    public void add(final T data) {
+        addLast(data);
+    }
+
+    /**
+     * Adds element to collection as first one.
+     * This means that calling {@link #getFirst()} immediately after this method will
+     * return element passed here and iterator returned by {@link #getIterator()}
+     * will return element passed here first if state of collection didn't change.
+     *
+     * @param data element to be added
+     */
+    public void addFirst(final T data) {
+        if (root == null) {
+            addFirstElement(data);
+        } else {
+            addElementAsFirst(data);
+        }
+        afterSuccessfullyAddedOneElement();
+    }
+
+    /**
+     * Adds element to collection as last one.
+     * This means that calling {@link #getLast()} immediately after this method will
+     * return element passed here and iterator returned by {@link #getIterator()}
+     * will return element passed here last if state of collection didn't change.
+     *
+     * @param data element to be added
+     */
+    public void addLast(final T data) {
+        if (root == null) {
+            addFirstElement(data);
+        } else {
+            addNextElement(data);
+        }
+        afterSuccessfullyAddedOneElement();
+    }
+
+    /**
+     * If collection is not empty then returns first element.
+     * If collection is empty throws NoSuchElementException
+     *
+     * @return first element in collection
+     * @throws NoSuchElementException if collection is empty
+     */
+    public T getFirst() {
+        if (root == null) {
+            throw new NoSuchElementException("Empty!");
+        }
+        return root.getData();
+    }
+
+    /**
+     * If collection is not empty then returns last element.
+     * If collection is empty throws NoSuchElementException
+     *
+     * @return last element in collection
+     * @throws NoSuchElementException if collection is empty
+     */
+    public T getLast() {
+        if (root == null) {
+            throw new NoSuchElementException("Empty!");
+        }
+        return lastNode.getData();
+    }
+
+    /**
+     * Removes first element from collection.
+     * Returns removed element.
+     *
+     * @return Element removed
+     * @throws NoSuchElementException if collection is empty
+     */
+    public T removeFirst() {
+        if (root == null) {
+            throw new NoSuchElementException("Empty!");
+        }
+
+        if (lastNode == root) {
+            return removeOnlyNode();
+        }
+
+        // Retrieve nodes to work on
+        DoubleLinkedListNode<T> currentFirst = root;
+        DoubleLinkedListNode<T> next = root.getNext();
+
+        // Unlink nodes
+        next.setPrevious(null);
+        currentFirst.setNext(null);
+
+        // Remove first
+        root = next;
+
+        // Change state - removed one node
+        size--;
+
+        // Return data from previously first node
+        return currentFirst.getData();
+    }
+
+    /**
+     * Removes last element from collection.
+     * Returns removed element.
+     *
+     * @return Element removed
+     * @throws NoSuchElementException if collection is empty
+     */
+    public T removeLast() {
+        if (root == null) {
+            throw new NoSuchElementException("Empty!");
+        }
+
+        if (lastNode == root) {
+            return removeOnlyNode();
+        }
+
+        // Retrieve nodes to work on
+        DoubleLinkedListNode<T> currentLast = lastNode;
+        DoubleLinkedListNode<T> previous = lastNode.getPrevious();
+
+        // Unlink nodes
+        previous.setNext(null);
+        currentLast.setPrevious(null);
+
+        // Remove first
+        lastNode = previous;
+
+        // Change state - removed one node
+        size--;
+
+        // Return data from previously last node
+        return currentLast.getData();
+    }
+
+    /**
+     * Removes element that matches one given if there is one in collection.
+     *
+     * @param data element to be removed from collection
+     * @return true if element was found and removed, false otherwise
+     */
+    public boolean removeFirstFound(final T data) {
+        if (root == null) {
+            return false;
+        }
+
+        DoubleLinkedListNode<T> currentNode = root;
+        while (currentNode != null) {
+            if (currentNode.getData() == null) {
+                if (data == null) {
+                    removeNode(currentNode);
+                    return true;
+                }
+            } else {
+                if (currentNode.getData().equals(data)) {
+                    removeNode(currentNode);
+                    return true;
+                }
+            }
+            currentNode = currentNode.getNext();
+        }
+        return false;
+    }
+
+    /**
+     * Returns number of elements held in collection.
+     *
+     * @return number of elements held in collection
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * Tells if collection holds any elements.
+     *
+     * @return true if collection has at least one element, false otherwise
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Returns iterator that iterates forward from first element to last
+     *
+     * @return iterator
+     */
+    public Iterator<T> getIterator() {
+        return new Iterator<T>() {
+            private boolean isBeforeRoot = true;
+            private DoubleLinkedListNode<T> currentNode = root;
+
+            @Override
+            public boolean hasNext() {
+
+                if (isBeforeRoot) {
+                    return root != null;
+                }
+
+                return currentNode.getNext() != null;
+            }
+
+
+            @Override
+            public T next() {
+                if (isBeforeRoot) {
+                    isBeforeRoot = false;
+                    if (currentNode == null) {
+                        throw new NoSuchElementException("Iterated out of bounds. Make sure that hasNext() returns true before calling this method.");
+                    }
+                    return currentNode.getData();
+                }
+                currentNode = currentNode.getNext();
+                if (currentNode == null) {
+                    throw new NoSuchElementException("Iterated out of bounds. Make sure that hasNext() returns true before calling this method.");
+                }
+                return currentNode.getData();
+            }
+        };
+    }
+
+
+    private T removeOnlyNode() {
+
+        // Save node to temporary variable
+        DoubleLinkedListNode<T> node = root;
+
+        // Clear only one node
+        root = lastNode = null;
+
+        // Change state - removed one node
+        size--;
+
+        // Return value
+        return node.getData();
+    }
+
     private void afterSuccessfullyAddedOneElement() {
         size++;
     }
@@ -20,9 +262,15 @@ public final class DoubleLinkedList<T> {
     }
 
     private void addNextElement(final T data) {
-        final DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<>(lastNode,data,null);
+        final DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<>(lastNode, data, null);
         lastNode.setNext(newNode);
         lastNode = newNode;
+    }
+
+    private void addElementAsFirst(T data) {
+        final DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<>(null, data, root);
+        root.setPrevious(newNode);
+        root = newNode;
     }
 
     private void removeNodeThatHasOnlyNext(final DoubleLinkedListNode<T> node, final DoubleLinkedListNode<T> next) {
@@ -31,7 +279,7 @@ public final class DoubleLinkedList<T> {
         root = next;
     }
 
-    private void removeNodeThatHasOnlyPrevious(final DoubleLinkedListNode<T> node,final DoubleLinkedListNode<T> prev) {
+    private void removeNodeThatHasOnlyPrevious(final DoubleLinkedListNode<T> node, final DoubleLinkedListNode<T> prev) {
         lastNode = lastNode.getPrevious();
 
         prev.setNext(null);
@@ -45,94 +293,24 @@ public final class DoubleLinkedList<T> {
 
     private void removeNode(final DoubleLinkedListNode<T> node) {
         DoubleLinkedListNode<T> prev = node.getPrevious();
-        DoubleLinkedListNode<T>next = node.getNext();
+        DoubleLinkedListNode<T> next = node.getNext();
 
         size--;
 
-        if(prev == null && next == null) {
+        if (prev == null && next == null) {
             root = null;
             return;
         }
 
-        if(prev == null) {
-            removeNodeThatHasOnlyNext(node,next);
+        if (prev == null) {
+            removeNodeThatHasOnlyNext(node, next);
 
-        } else if(next == null) {
-            removeNodeThatHasOnlyPrevious(node,prev);
+        } else if (next == null) {
+            removeNodeThatHasOnlyPrevious(node, prev);
 
         } else {
-            removeNodeThatIsInMiddle(prev,next);
+            removeNodeThatIsInMiddle(prev, next);
 
         }
-    }
-
-    public void add(final T data) {
-        if(root == null) {
-            addFirstElement(data);
-        } else {
-            addNextElement(data);
-        }
-        afterSuccessfullyAddedOneElement();
-    }
-
-    public boolean removeFirst(final T data) {
-        if(root == null) {
-            return false;
-        }
-
-        DoubleLinkedListNode<T> currentNode = root;
-        while(currentNode != null) {
-            if(currentNode.getData() == null) {
-                if(data == null) {
-                    removeNode(currentNode);
-                    return true;
-                }
-            } else {
-                if(currentNode.getData().equals(data)) {
-                    removeNode(currentNode);
-                    return true;
-                }
-            }
-            currentNode = currentNode.getNext();
-        }
-        return false;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public Iterator<T> getIterator() {
-        return new Iterator<T>() {
-            private boolean isBeforeRoot = true;
-            private DoubleLinkedListNode<T> currentNode = root;
-
-            @Override
-            public boolean hasNext() {
-
-                if(isBeforeRoot) {
-                    return root!=null;
-                }
-
-                return currentNode.getNext() != null;
-            }
-
-           
-            @Override
-            public T next() {
-                if(isBeforeRoot) {
-                    isBeforeRoot = false;
-                    if(currentNode == null) {
-                        throw new NoSuchElementException("Iterated out of bounds. Make sure that hasNext() returns true before calling this method.");
-                    }
-                    return currentNode.getData();
-                }
-                currentNode = currentNode.getNext();
-                if(currentNode == null) {
-                    throw new NoSuchElementException("Iterated out of bounds. Make sure that hasNext() returns true before calling this method.");
-                }
-                return currentNode.getData();
-            }
-        };
     }
 }
--- src/test/java/pl/nosystems/java/datastructures/list/DoubleLinkedListTest.java
+++ src/test/java/pl/nosystems/java/datastructures/list/DoubleLinkedListTest.java
@@ -9,8 +9,7 @@ import java.util.NoSuchElementException;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public final class DoubleLinkedListTest {
     private DoubleLinkedList<Integer> list;
@@ -21,38 +20,258 @@ public final class DoubleLinkedListTest {
         list = new DoubleLinkedList<>();
     }
 
+
+    @Test(expected = NoSuchElementException.class)
+    public void givenNoElementsWhenRequestedFirstShouldThrow() {
+
+        // Given - When - Then
+        list.getFirst();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void givenNoElementsWhenRequestedLastShouldThrow() {
+
+        // Given - When - Then
+        list.getLast();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void givenNoElementsWhenRequestedToRemoveFirstShouldThrow() {
+
+        // Given - When - Then
+        list.removeFirst();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void givenNoElementsWhenRequestedToRemoveLastShouldThrow() {
+
+        // Given - When - Then
+        list.removeLast();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iterateAfterHasNextReturnsFalseOnEmptyListTest() {
+        final Iterator<Integer> iterator = list.getIterator();
+        iterator.next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iterateAfterHasNextReturnsFalseOnNonEmptyListTest() {
+        list.add(VALUE);
+        final Iterator<Integer> iterator = list.getIterator();
+        iterator.next();
+        iterator.next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void addThreeRemoveInMiddleIterateAfterHasNextReturnedFalseTest() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        assertTrue(list.removeFirstFound(2));
+
+        final Iterator<Integer> iterator = list.getIterator();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+    }
+
+
     @Test
     public void noElementsTest() {
-        assertThat(list.getSize(),is(equalTo(0)));
+        assertThat(list.getSize(), is(equalTo(0)));
         final Iterator<Integer> iterator = list.getIterator();
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void givenOneElementWhenRequestedFirstShouldReturnIt() {
+
+        // Given
+        Integer element = 1;
+        list.add(element);
+
+        // When
+        Integer elementReturned = list.getFirst();
+
+        // Then verify
+        assertSame(element, elementReturned);
+    }
+
+    @Test
+    public void givenOneElementWhenRequestedLastShouldReturnIt() {
+
+        // Given
+        Integer element = 1;
+        list.add(element);
+
+        // When
+        Integer elementReturned = list.getLast();
+
+        // Then verify
+        assertSame(element, elementReturned);
+    }
+
+    @Test
+    public void givenMultipleElementsWhenRequestedByPositionShouldReturnThem() {
+
+        // Given
+        Integer element1 = 1;
+        Integer element2 = 2;
+        Integer element3 = 3;
+
+        list.add(element1);
+        list.add(element2);
+        list.add(element3);
+
+
+        // When
+        Integer first = list.getFirst();
+        Integer last = list.getLast();
+
+        // Then verify
+        assertSame(element1, first);
+        assertSame(element3, last);
+    }
+
+    @Test
+    public void givenMultipleElementsWhenRemovedByRemoveLastShouldReallyRemoveLast() {
+
+        // Given
+        Integer element1 = 1;
+        Integer element2 = 2;
+        Integer element3 = 3;
+
+        list.add(element1);
+        list.add(element2);
+        list.add(element3);
+
+
+        // When
+        Integer removed1 = list.removeLast();
+        Integer removed2 = list.removeLast();
+        Integer removed3 = list.removeLast();
+
+        // Then verify
+        assertSame(element3, removed1);
+        assertSame(element2, removed2);
+        assertSame(element1, removed3);
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void givenMultipleElementsWhenRemovedByRemoveFirstShouldReallyRemoveFirst() {
+
+        // Given
+        Integer element1 = 1;
+        Integer element2 = 2;
+        Integer element3 = 3;
+
+        list.add(element1);
+        list.add(element2);
+        list.add(element3);
+
+
+        // When
+        Integer removed1 = list.removeFirst();
+        Integer removed2 = list.removeFirst();
+        Integer removed3 = list.removeFirst();
+
+        // Then verify
+        assertSame(element1, removed1);
+        assertSame(element2, removed2);
+        assertSame(element3, removed3);
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void addFirstShouldAddFirstTest() {
+
+        // Given
+        Integer element1 = 1;
+        Integer element2 = 2;
+        Integer element3 = 3;
+
+
+        // When
+        list.addFirst(element1);
+        list.addFirst(element2);
+        list.addFirst(element3);
+
+
+        // Then Verify
+        Integer first = list.getFirst();
+        Integer last = list.getLast();
+
+        assertSame(element3, first);
+        assertSame(element1, last);
+        assertFalse(list.isEmpty());
+        assertEquals(3, list.getSize());
+
+        Iterator<Integer> iterator = list.getIterator();
+        assertSame(element3, iterator.next());
+        assertSame(element2, iterator.next());
+        assertSame(element1, iterator.next());
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void addLastShouldAddLastTest() {
+
+        // Given
+        Integer element1 = 1;
+        Integer element2 = 2;
+        Integer element3 = 3;
+
+
+        // When
+        list.addLast(element1);
+        list.addLast(element2);
+        list.addLast(element3);
+
+
+        // Then Verify
+        Integer first = list.getFirst();
+        Integer last = list.getLast();
+
+        assertSame(element1, first);
+        assertSame(element3, last);
+        assertFalse(list.isEmpty());
+        assertEquals(3, list.getSize());
+
+        Iterator<Integer> iterator = list.getIterator();
+        assertSame(element1, iterator.next());
+        assertSame(element2, iterator.next());
+        assertSame(element3, iterator.next());
         assertFalse(iterator.hasNext());
     }
 
     @Test
     public void addOneElementTest() {
         list.add(VALUE);
-        assertThat(list.getSize(),is(equalTo(1)));
+        assertThat(list.getSize(), is(equalTo(1)));
         final Iterator<Integer> iterator = list.getIterator();
         assertTrue(iterator.hasNext());
         final Integer data = iterator.next();
-        assertThat(data,is(equalTo(VALUE)));
+        assertThat(data, is(equalTo(VALUE)));
         assertFalse(iterator.hasNext());
     }
 
     @Test
     public void addTwoElementsTest() {
         list.add(VALUE);
-        list.add(VALUE+1);
-        assertThat(list.getSize(),is(equalTo(2)));
+        list.add(VALUE + 1);
+        assertThat(list.getSize(), is(equalTo(2)));
 
         final Iterator<Integer> iterator = list.getIterator();
         assertTrue(iterator.hasNext());
         Integer data = iterator.next();
-        assertThat(data,is(equalTo(VALUE)));
+        assertThat(data, is(equalTo(VALUE)));
         assertTrue(iterator.hasNext());
 
         data = iterator.next();
-        assertThat(data,is(equalTo(VALUE+1)));
+        assertThat(data, is(equalTo(VALUE + 1)));
         assertFalse(iterator.hasNext());
     }
 
@@ -61,13 +280,13 @@ public final class DoubleLinkedListTest {
         for (int i = 0; i < 100; i++) {
             list.add(i);
         }
-        assertThat(list.getSize(),is(equalTo(100)));
+        assertThat(list.getSize(), is(equalTo(100)));
 
         final Iterator<Integer> iterator = list.getIterator();
         for (int i = 0; i < 100; i++) {
             assertTrue(iterator.hasNext());
             final Integer data = iterator.next();
-            assertThat(data,is(equalTo(i)));
+            assertThat(data, is(equalTo(i)));
         }
 
         assertFalse(iterator.hasNext());
@@ -75,8 +294,8 @@ public final class DoubleLinkedListTest {
 
     @Test
     public void addNoneRemoveOneTest() {
-        assertFalse(list.removeFirst(VALUE));
-        assertThat(list.getSize(),is(equalTo(0)));
+        assertFalse(list.removeFirstFound(VALUE));
+        assertThat(list.getSize(), is(equalTo(0)));
         final Iterator<Integer> iterator = list.getIterator();
         assertFalse(iterator.hasNext());
     }
@@ -84,9 +303,9 @@ public final class DoubleLinkedListTest {
     @Test
     public void addOneRemoveOneTest() {
         list.add(VALUE);
-        assertTrue(list.removeFirst(VALUE));
+        assertTrue(list.removeFirstFound(VALUE));
 
-        assertThat(list.getSize(),is(equalTo(0)));
+        assertThat(list.getSize(), is(equalTo(0)));
         final Iterator<Integer> iterator = list.getIterator();
         assertFalse(iterator.hasNext());
     }
@@ -95,15 +314,15 @@ public final class DoubleLinkedListTest {
     public void addTwoRemoveOneTest() {
         list.add(VALUE);
         list.add(VALUE);
-        assertTrue(list.removeFirst(VALUE));
+        assertTrue(list.removeFirstFound(VALUE));
 
-        assertThat(list.getSize(),is(equalTo(1)));
+        assertThat(list.getSize(), is(equalTo(1)));
 
         Iterator<Integer> iterator = list.getIterator();
         assertTrue(iterator.hasNext());
 
-        assertTrue(list.removeFirst(VALUE));
-        assertThat(list.getSize(),is(equalTo(0)));
+        assertTrue(list.removeFirstFound(VALUE));
+        assertThat(list.getSize(), is(equalTo(0)));
 
         iterator = list.getIterator();
         assertFalse(iterator.hasNext());
@@ -113,9 +332,9 @@ public final class DoubleLinkedListTest {
     public void addTwoRemoveNonExistentTest() {
         list.add(VALUE);
         list.add(VALUE);
-        assertFalse(list.removeFirst(VALUE+1));
+        assertFalse(list.removeFirstFound(VALUE + 1));
 
-        assertThat(list.getSize(),is(equalTo(2)));
+        assertThat(list.getSize(), is(equalTo(2)));
 
         Iterator<Integer> iterator = list.getIterator();
         assertTrue(iterator.hasNext());
@@ -132,9 +351,9 @@ public final class DoubleLinkedListTest {
         list.add(VALUE);
         list.add(VALUE);
         list.add(VALUE);
-        assertFalse(list.removeFirst(VALUE+1));
+        assertFalse(list.removeFirstFound(VALUE + 1));
 
-        assertThat(list.getSize(),is(equalTo(3)));
+        assertThat(list.getSize(), is(equalTo(3)));
 
         Iterator<Integer> iterator = list.getIterator();
         assertTrue(iterator.hasNext());
@@ -156,10 +375,10 @@ public final class DoubleLinkedListTest {
         }
 
         for (int i = 0; i < 100; i++) {
-            assertTrue(list.removeFirst(VALUE));
+            assertTrue(list.removeFirstFound(VALUE));
         }
 
-        assertThat(list.getSize(),is(equalTo(0)));
+        assertThat(list.getSize(), is(equalTo(0)));
 
         final Iterator<Integer> iterator = list.getIterator();
         assertFalse(iterator.hasNext());
@@ -172,10 +391,10 @@ public final class DoubleLinkedListTest {
         }
 
         for (int i = 99; i >= 0; i--) {
-            assertTrue(list.removeFirst(i));
+            assertTrue(list.removeFirstFound(i));
         }
 
-        assertThat(list.getSize(),is(equalTo(0)));
+        assertThat(list.getSize(), is(equalTo(0)));
 
         final Iterator<Integer> iterator = list.getIterator();
         assertFalse(iterator.hasNext());
@@ -188,13 +407,13 @@ public final class DoubleLinkedListTest {
         }
 
         for (int i = 50; i >= 0; i--) {
-            assertTrue(list.removeFirst(i));
+            assertTrue(list.removeFirstFound(i));
         }
-        for (int i = 51; i < 100 ; i++) {
-            assertTrue(list.removeFirst(i));
+        for (int i = 51; i < 100; i++) {
+            assertTrue(list.removeFirstFound(i));
         }
 
-        assertThat(list.getSize(),is(equalTo(0)));
+        assertThat(list.getSize(), is(equalTo(0)));
 
         final Iterator<Integer> iterator = list.getIterator();
         assertFalse(iterator.hasNext());
@@ -205,13 +424,34 @@ public final class DoubleLinkedListTest {
         list.add(null);
         list.add(null);
         list.add(null);
-        assertTrue(list.removeFirst(null));
-        assertTrue(list.removeFirst(null));
-        assertTrue(list.removeFirst(null));
+        assertTrue(list.removeFirstFound(null));
+        assertTrue(list.removeFirstFound(null));
+        assertTrue(list.removeFirstFound(null));
 
-        assertThat(list.getSize(),is(equalTo(0)));
+        assertThat(list.getSize(), is(equalTo(0)));
 
         Iterator<Integer> iterator = list.getIterator();
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void addThreeRemoveInMiddleTest() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        assertTrue(list.removeFirstFound(2));
+
+        assertThat(list.getSize(), is(equalTo(2)));
+
+        final Iterator<Integer> iterator = list.getIterator();
+
+        assertTrue(iterator.hasNext());
+        assertThat(iterator.next(), is(equalTo(1)));
+
+        assertTrue(iterator.hasNext());
+        assertThat(iterator.next(), is(equalTo(3)));
+
         assertFalse(iterator.hasNext());
     }
 
@@ -221,9 +461,9 @@ public final class DoubleLinkedListTest {
         list.add(null);
         list.add(null);
 
-        assertFalse(list.removeFirst(VALUE));
+        assertFalse(list.removeFirstFound(VALUE));
 
-        assertThat(list.getSize(),is(equalTo(3)));
+        assertThat(list.getSize(), is(equalTo(3)));
 
         final Iterator<Integer> iterator = list.getIterator();
         assertTrue(iterator.hasNext());
@@ -236,54 +476,5 @@ public final class DoubleLinkedListTest {
 
         iterator.next();
         assertFalse(iterator.hasNext());
-    }
-
-    @Test(expected = NoSuchElementException.class)
-    public void iterateAfterHasNextReturnsFalseOnEmptyListTest() {
-        final Iterator<Integer> iterator = list.getIterator();
-        iterator.next();
-    }
-
-    @Test(expected = NoSuchElementException.class)
-    public void iterateAfterHasNextReturnsFalseOnNonEmptyListTest() {
-        list.add(VALUE);
-        final Iterator<Integer> iterator = list.getIterator();
-        iterator.next();
-        iterator.next();
-    }
-
-    @Test
-    public void addThreeRemoveInMiddleTest() {
-        list.add(1);
-        list.add(2);
-        list.add(3);
-
-        assertTrue(list.removeFirst(2));
-
-        assertThat(list.getSize(),is(equalTo(2)));
-
-        final Iterator<Integer> iterator = list.getIterator();
-
-        assertTrue(iterator.hasNext());
-        assertThat(iterator.next(),is(equalTo(1)));
-
-        assertTrue(iterator.hasNext());
-        assertThat(iterator.next(),is(equalTo(3)));
-
-        assertFalse(iterator.hasNext());
-    }
-
-    @Test(expected = NoSuchElementException.class)
-    public void addThreeRemoveInMiddleIterateAfterHasNextReturnedFalseTest() {
-        list.add(1);
-        list.add(2);
-        list.add(3);
-
-        assertTrue(list.removeFirst(2));
-
-        final Iterator<Integer> iterator = list.getIterator();
-        iterator.next();
-        iterator.next();
-        iterator.next();
     }
 }
